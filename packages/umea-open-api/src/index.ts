8e187c8ae7de6aaf03aa588d9de09f8f764d2141
arvidbt <arvid.bergman@sap.com>
2023-06-27 15:38:54 +0200

    removed main function

--- packages/umea-open-api/src/index.ts
+++ packages/umea-open-api/src/index.ts
@@ -8,19 +8,3 @@ export class UmeaOpenAPI {
     return res.records.map(recordToBeach)
   }
 }
-
-async function main() {
-  const api = new UmeaOpenAPI()
-  const res = await api.getPublicBeaches({
-    beachProximityFilter: {
-      coordinate: {
-        lat: 63.84423982962995,
-        lon: 20.280075615518985 
-      },
-      distance: 5000,
-    }
-  })
-  console.log(res)
-}
-
-main()
