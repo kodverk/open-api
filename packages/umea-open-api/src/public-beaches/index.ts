720a58fb8f3762e1fec82613c7fae8af09399e64
Viktor Malmedal <viktor@malmedal.dev>
2023-06-27 15:52:56 +0200

    misc: format

--- packages/umea-open-api/src/public-beaches/index.ts
+++ packages/umea-open-api/src/public-beaches/index.ts
@@ -1,44 +1,53 @@
 import { typedFetch } from "../utils/typed-fetch";
-import type { PublicBeachesIncomingResponse, Record } from "./types/incoming-response";
-import type { Beach } from "./types/outgoing-response"
+import type {
+  BeachRecord,
+  PublicBeachesIncomingResponse,
+} from "./types/incoming-response";
+import type { Beach, Coordinate } from "./types/outgoing-response";
 
-const baseUrl = "https://opendata.umea.se/api/records/1.0/search"
+const baseUrl = "https://opendata.umea.se/api/records/1.0/search";
 
 type BeachProximityFilter = {
-    coordinate: {
-      lat: number
-      lon: number
-    }
-    distance: number
-}
+  coordinate: Coordinate;
+  distance: number;
+};
 
 export type FetchPublicBeachesOptions = {
-  searchQuery?: string
-  rows?: number
-  beachProximityFilter?: BeachProximityFilter
-}
+  searchQuery?: string;
+  rows?: number;
+  beachProximityFilter?: BeachProximityFilter;
+};
 
-const translateProximityFilterToParam = ({coordinate: {lat, lon}, distance}: BeachProximityFilter) => [lat, lon, distance].join(",")
+const translateProximityFilterToParam = ({
+  coordinate: { lat, lon },
+  distance,
+}: BeachProximityFilter) => [lat, lon, distance].join(",");
 
 export function fetchPublicBeaches(options: FetchPublicBeachesOptions) {
-  const {searchQuery, beachProximityFilter, rows} = options
-  const params = new URLSearchParams({dataset: "badplatser"})
-  if(searchQuery) params.append("q", searchQuery)
-  if(rows) params.append("rows", rows.toString())
-  if(beachProximityFilter) params.append("geofilter.distance", translateProximityFilterToParam(beachProximityFilter))
-  console.log({url:`${baseUrl}?${params}`})
-  return typedFetch<PublicBeachesIncomingResponse>(`${baseUrl}?${params}`)
+  const { searchQuery, beachProximityFilter, rows } = options;
+  const params = new URLSearchParams({ dataset: "badplatser" });
+  if (searchQuery) params.append("q", searchQuery);
+  if (rows) params.append("rows", rows.toString());
+  if (beachProximityFilter)
+    params.append(
+      "geofilter.distance",
+      translateProximityFilterToParam(beachProximityFilter),
+    );
+  return typedFetch<PublicBeachesIncomingResponse>(
+    `${baseUrl}?${params.toString()}`,
+  );
 }
 
-export function recordToBeach(beach: Record) {
+export function recordToBeach(beach: BeachRecord) {
+  const [lat, lon] = beach.geometry.coordinates;
   return {
     id: beach.recordid,
     name: beach.fields.namn,
     area: beach.fields.omrade,
     facilityId: beach.fields.anlaggn_id,
     timestamp: beach.record_timestamp,
-    geometry: beach.geometry,
+    coordinate: lat && lon ? { lat, lon } : undefined,
     handicapAccesible: !!beach.fields?.handik_anp,
-    managedBy: beach.fields?.huvudansva ?? null,
-  } satisfies Beach
+    managedBy: beach.fields?.huvudansva,
+  } satisfies Beach;
 }
