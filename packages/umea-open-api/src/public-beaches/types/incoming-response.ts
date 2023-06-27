8258e7c0786f640537aa3ac400a7406909c94b64
Viktor Malmedal <viktor@malmedal.dev>
2023-06-27 15:53:10 +0200

    misc: format

--- packages/umea-open-api/src/public-beaches/types/incoming-response.ts
+++ packages/umea-open-api/src/public-beaches/types/incoming-response.ts
@@ -1,50 +1,50 @@
 export interface PublicBeachesIncomingResponse {
-  nhits: number
-  parameters: Parameters
-  records: Record[]
-  facet_groups: FacetGroup[]
+  nhits: number;
+  parameters: Parameters;
+  records: BeachRecord[];
+  facet_groups: FacetGroup[];
 }
 
 export interface Parameters {
-  dataset: string
-  rows: number
-  start: number
-  facet: string[]
-  format: string
-  timezone: string
+  dataset: string;
+  rows: number;
+  start: number;
+  facet: string[];
+  format: string;
+  timezone: string;
 }
 
-export interface Record {
-  datasetid: string
-  recordid: string
-  fields: Fields
-  geometry: Geometry
-  record_timestamp: string
+export interface BeachRecord {
+  datasetid: string;
+  recordid: string;
+  fields: Fields;
+  geometry: Geometry;
+  record_timestamp: string;
 }
 
 export interface Fields {
-  namn: string
-  geo_point_2d: number[]
-  anlaggn_id: string
-  omrade: string
-  handik_anp?: string
-  typ: string
-  huvudansva?: string
+  namn: string;
+  geo_point_2d: number[];
+  anlaggn_id: string;
+  omrade: string;
+  handik_anp?: string;
+  typ: string;
+  huvudansva?: string;
 }
 
 export interface Geometry {
-  type: string
-  coordinates: number[]
+  type: string;
+  coordinates: number[];
 }
 
 export interface FacetGroup {
-  name: string
-  facets: Facet[]
+  name: string;
+  facets: Facet[];
 }
 
 export interface Facet {
-  name: string
-  count: number
-  state: string
-  path: string
+  name: string;
+  count: number;
+  state: string;
+  path: string;
 }
