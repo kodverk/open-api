0bc58221789d7b9092b0722d91224fe15df5ed18
Viktor Malmedal <viktor@malmedal.dev>
2023-06-28 00:00:22 +0200

    wip: traverase unist tree...

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -4,6 +4,7 @@
 import withMdx from "@next/mdx";
 import rehypePrettyCode from "rehype-pretty-code";
 import { getHighlighter } from "shiki";
+import { visit } from "unist-util-visit";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -18,6 +19,19 @@ const nextConfig = {
 export default withMdx({
   options: {
     rehypePlugins: [
+      () => (tree) => {
+        visit(tree, (node) => {
+          if (node?.type === "element" && node?.tagName === "pre") {
+            const [codeEl] = node.children;
+            if (codeEl.tagName !== "code") {
+              return;
+            }
+            if (node.__rawString__ === undefined) {
+              node.__rawString__ = codeEl.children?.[0].value;
+            }
+          }
+        });
+      },
       [
         rehypePrettyCode,
         /** @type {import("rehype-pretty-code").Options} */
@@ -40,6 +54,34 @@ export default withMdx({
           },
         }),
       ],
+      () => (tree) => {
+        visit(tree, (node) => {
+          if (node?.type === "element" && node?.tagName === "div") {
+            if (!("data-rehype-pretty-code-fragment" in node.properties)) {
+              return;
+            }
+            const preElement = node.children.at(-1);
+            if (preElement.tagName !== "pre") {
+              return;
+            }
+            preElement.properties["__rawString__"] = node.__rawString__;
+            if (node.__rawString__.startsWith("npm install")) {
+              const npmCommand = node.__rawString__;
+              preElement.properties["__npmCommand__"] = npmCommand;
+              preElement.properties["__yarnCommand__"] = npmCommand.replace(
+                "npm install",
+                "yarn add",
+              );
+              preElement.properties["__pnpmCommand__"] = npmCommand.replace(
+                "npm install",
+                "pnpm add",
+              );
+            }
+
+            console.log(preElement.properties);
+          }
+        });
+      },
     ],
   },
 })(nextConfig);
